cf605a341653f2cb2cf5275f65b2a56a43eacb6e
scammi <scammi@gmail.com>
2025-07-21 17:23:24 +0200

    fix(core): use webpack 5 on dashboard

--- packages/dashboard/next.config.js
+++ packages/dashboard/next.config.js
@@ -4,28 +4,36 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
-  webpack5: false,
+  webpack5: true,
   // Use the prefix in production and not development.
   assetPrefix: isProd ? '' : '',
   // https://nextjs.org/docs/messages/import-esm-externals
   experimental: { esmExternals: 'loose', outputFileTracing: true },
   // see https://github.com/vercel/vercel/issues/2569#issuecomment-514865342
   webpack(config, { isServer }) {
+    // Add fallbacks for node modules
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      zlib: require.resolve('browserify-zlib'),
+      path: require.resolve('path-browserify'),
+      fs: false, // fs cannot be polyfilled
+    };
     if (isServer) {
       config.resolve.mainFields = ['module', 'main'];
       // Fix all packages that this change breaks:
       config.resolve.alias['node-fetch'] = 'node-fetch/lib/index.js';
-      const [nextJsExternals] = config.externals;
-      config.externals = [
-        (context, request, callback) => {
-          // Ignore electron, it is referenced in cas-ipfs/node_modules/electron-fetch
-          const IGNORES = ['electron'];
-          if (IGNORES.indexOf(request) >= 0) {
-            return callback(null, "require('" + request + "')");
-          }
-          nextJsExternals(context, request, callback);
-        },
-      ];
+      // Add a new externals function to handle the 'electron' package,
+      // instead of replacing the existing externals array.
+      config.externals.push((context, request, callback) => {
+        if (request === 'electron') {
+          return callback(null, `require('${request}')`);
+        }
+        return callback();
+      });
     }
     return config;
   },
